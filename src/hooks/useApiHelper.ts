a68b1ac354c9e9813579112ccf153351070b77b2
vikramsamak <vikramsamak02@gmail.com>
2024-12-25 23:52:24 +0530

    refactor: enhance useApiHelper to support mutations and improve type definitions

--- src/hooks/useApiHelper.ts
+++ src/hooks/useApiHelper.ts
@@ -1,25 +1,49 @@
-import { useQuery, UseQueryResult } from "@tanstack/react-query";
-import { useApiHelperProps } from "../types";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { ApiHelperResult, useApiHelperProps } from "../types";
 import { apiRequest } from "../utils";
 import { useApiHelperContext } from "./useApiHelperContext";
 
-export function useApiHelper<responseType = unknown, errorType = unknown>({
+export function useApiHelper<
+  responseType = unknown,
+  errorType = unknown,
+  payloadType = unknown
+>({
   url,
   params,
-  queryKey,
+  queryKey = [],
   axiosOptions,
   method,
+  onSuccess,
+  onError,
   ...rest
-}: useApiHelperProps<responseType, errorType>): UseQueryResult<responseType, errorType> {
+}: useApiHelperProps<responseType, errorType, payloadType>): ApiHelperResult<
+  responseType,
+  errorType,
+  payloadType
+> {
   const { axiosInstance } = useApiHelperContext();
 
-  return useQuery<responseType, errorType>({
-    queryKey: queryKey,
+  const queryResult = useQuery<responseType, errorType>({
+    queryKey: queryKey || [],
     queryFn: async () =>
-      await apiRequest<responseType, errorType>(axiosInstance, method, url, {
+      await apiRequest<responseType, errorType>(axiosInstance, "GET", url, {
         params,
         ...axiosOptions,
       }),
+    enabled: method === "GET",
     ...rest,
   });
+
+  const mutationResult = useMutation<responseType, errorType, payloadType>({
+    mutationFn: async (payload: payloadType) =>
+      await apiRequest<responseType, errorType>(axiosInstance, method, url, {
+        data: payload,
+        params,
+        ...axiosOptions,
+      }),
+    onSuccess,
+    onError,
+  });
+
+  return method === "GET" ? queryResult : mutationResult;
 }
--- src/types/useApiHelper.types.ts
+++ src/types/useApiHelper.types.ts
@@ -2,11 +2,20 @@ import { QueryKey, UseQueryOptions } from "@tanstack/react-query";
 import { AxiosRequestConfig } from "axios";
 import { HTTPMethod } from "./GenericTypes";
 
-export interface useApiHelperProps<responseType, errorType>
-  extends Omit<UseQueryOptions<responseType, errorType, responseType>, "queryKey" | "queryFn"> {
+export interface useApiHelperProps<
+  responseType,
+  errorType,
+  payloadType = unknown
+> extends Omit<
+    UseQueryOptions<responseType, errorType, responseType>,
+    "queryKey" | "queryFn"
+  > {
   url: string;
   params?: Record<string, unknown>;
+  data?: payloadType;
   queryKey: QueryKey;
   axiosOptions?: AxiosRequestConfig;
   method: HTTPMethod;
+  onSuccess?: (data: responseType) => void;
+  onError?: (error: errorType) => void;
 }
