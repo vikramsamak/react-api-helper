d5e84c9dcef16e1991d8e58eb501cabd194ee780
vikramsamak <vikramsamak02@gmail.com>
2025-01-01 21:59:36 +0530

    refactor: remove unused imports and clean up GenericTypes.ts

--- lib/types/GenericTypes.ts
+++ lib/types/GenericTypes.ts
@@ -1,7 +1,1 @@
-import { UseMutationResult, UseQueryResult } from '@tanstack/react-query';
-
 export type HTTPMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
-
-export type ApiHelperResult<responseType, errorType, payloadType> =
-  | UseQueryResult<responseType, errorType>
-  | UseMutationResult<responseType, errorType, payloadType>;
